32e56f6ccc09aa871df3bf5402665f410ae88825
Mrelshimy <mraafat.elsayed@gmail.com>
2024-04-05 18:05:19 +0200

    Task 3 updates

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$('document').ready(function () {
+  const selectedAmenities = {};
+
+  $('.amenities input').click(function () {
+    if (this.checked) {
+      selectedAmenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete selectedAmenities[this.dataset.name];
+    }
+    $('.amenities h4').text(Object.keys(selectedAmenities));
+    if ($.isEmptyObject(selectedAmenities)) {
+      $('.amenities h4').html('&nbsp;');
+    }
+  });
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (body) => {
+    if (body.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
