a28806aa5325053fe09d9201b6e43f365faab700
Sameh ElGazar <selgazar95@gmail.com>
2024-04-05 06:14:05 +0200

    Task 0, 1 & Part of 2

---
+++ web_dynamic/scripts/1-hbnb.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+    let selectedAmenities = [];
+    
+    $('.amenities ul li input:checkbox')
+
+    // Will complete it later
+
+    // $('.amenities h4').append();
+})
