d29cd6813cb99c645d6f1171ed8f44200e493777
Sameh ElGazar <selgazar95@gmail.com>
2024-04-07 05:27:52 +0200

    Done 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -27,40 +27,8 @@ $('document').ready(function () {
     first will check if the user filtered the amenities,
     it will add ids of those amenities to the filteredIds object, then clear the places' previous children
     and then pass this filteredIds to the ajax url and api route will handle the filtering,
-    then getPlaceArticle() will create place for each amenity id and appends it to places
+    then create the article and append it to places
   */
-  const filteredIds = {};
-  function getPlaceArticle(place) {
-    /*
-      This func takes place object as param then get the user that has relation with this specific space,
-      then create the article using those place and user objects and return it.
-    */
-    const user = $.get(`http://0.0.0.0:5001/api/v1/users/${place.user_id}`);
-    const placeArticle = $(`
-            <article>
-              <div class="title_box">
-                <h2>${place.name}</h2>
-                <div class="price_by_night">$ ${place.price_by_night}</div>
-              </div>
-              <div class="information">
-                  <div class="max_guest">${place.max_guest} Guest${
-      place.max_guest != 1 ? 's' : ''
-    }</div>
-                  <div class="number_rooms">${place.number_rooms} Bedroom${
-      place.number_rooms != 1 ? 's' : ''
-    }</div>
-                  <div class="number_bathrooms">${
-                    place.number_bathrooms
-                  } Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>
-              </div>
-              <div class="user">
-                  <b>Owner: </b>${user.first_name} ${user.last_name}
-              </div>
-              <div class="description">${place.description}</div>
-            </article>
-          `);
-    return placeArticle;
-  }
 
   $('.filters button').click(function (event) {
     event.preventDefault();
@@ -76,7 +44,34 @@ $('document').ready(function () {
     })
       .done(function (data) {
         $.each(data, function (idx, place) {
-          $('.places').append(getPlaceArticle(place));
+          $.get(
+            `http://0.0.0.0:5001/api/v1/users/${place.user_id}`,
+            function (user) {
+              $(`
+            <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$ ${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                  <div class="max_guest">${place.max_guest} Guest${
+                place.max_guest != 1 ? 's' : ''
+              }</div>
+                  <div class="number_rooms">${place.number_rooms} Bedroom${
+                place.number_rooms != 1 ? 's' : ''
+              }</div>
+                  <div class="number_bathrooms">${
+                    place.number_bathrooms
+                  } Bathroom${place.number_bathrooms != 1 ? 's' : ''}</div>
+              </div>
+              <div class="user">
+                  <b>Owner: </b>${user.first_name} ${user.last_name}
+              </div>
+              <div class="description">${place.description}</div>
+            </article>
+          `).appendTo('.places');
+            }
+          );
         });
       })
       .fail(function (xhr, status) {
